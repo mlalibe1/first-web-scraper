5cc98d949c89fe615ebdf479424944760d16a50c
Marissa Laliberte <marissalaliberte@Marissas-MacBook-Pro.local>
2016-04-13 14:24:10 -0400

    added april 13 assignment

--- scrapers/press1.py
+++ scrapers/press1.py
@@ -12,15 +12,15 @@ soup = BeautifulSoup(html)
 table = soup.findAll('table')[0]
 
 list_of_rows = []
-for row in table.findAll('tr')[1:]:
+for row in table.findAll('tr')
     list_of_cells = []
-    for cell in row.findAll('td'): 
+    for cell in row.findAll('td'):[1] 
         list_of_cells.append(cell.text)
         if cell.find('a'):
-            list_of_cells.append('http://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html' + cell.find('a')['href'])
+            list_of_cells.append('http://www.tdcj.state.tx.us/death_row/dr_info/' + cell.find('a')['href'])
     list_of_rows.append(list_of_cells)
     
 outfile = open('./releases.csv', 'wb')
 writer = csv.writer(outfile)
-writer.writerow(['date', 'title', 'url'])
+writer.writerow(['scheduled execution', 'link', 'last name', 'first name', 'TDCJ number', 'date of birth', 'race', 'date received', 'county'])
 writer.writerows(list_of_rows)
